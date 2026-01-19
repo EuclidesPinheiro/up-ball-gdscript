4414e6c6f875b149b3370fbcc7ff218ffe60ad73
Euclides Pereira <kid.pereira55@gmail.com>
2026-01-19 16:17:19 -0300

    fixing some perfomance issues

--- Scripts/Entities/star_collectible.gd
+++ Scripts/Entities/star_collectible.gd
@@ -7,8 +7,10 @@ extends Area2D
 @export var speed: float = 150.0
 @export var rotation_speed: float = 2.0
 
-# Animation
-var _pulse_tween: Tween
+# Animation - using simple sin wave instead of Tween for performance
+var _time: float = 0.0
+var _base_scale: Vector2 = Vector2.ONE
+var _is_collected: bool = false
 
 signal collected
 
@@ -16,40 +18,38 @@ signal collected
 func _ready() -> void:
 	# Connect body entered signal
 	body_entered.connect(_on_body_entered)
-	
-	# Start pulse animation
-	_start_pulse_animation()
-	
-	# Start rotation
-	set_process(true)
-
-
-func _process(delta: float) -> void:
-	# Rotate the star for visual effect
-	rotation += rotation_speed * delta
+	_base_scale = scale
 
 
 func _physics_process(delta: float) -> void:
+	if _is_collected:
+		return
+	
 	# Move down the screen
 	position += Vector2(0, speed * delta)
+	
+	# Rotate the star for visual effect
+	rotation += rotation_speed * delta
+	
+	# Simple pulse animation using sin wave (much more performant than Tween loops)
+	_time += delta * 4.0  # Speed of pulse
+	var pulse_scale: float = 1.0 + sin(_time) * 0.075  # 7.5% scale variation
+	scale = _base_scale * pulse_scale
 	
 	# Remove if off screen
 	if position.y > 1400:
 		queue_free()
 
 
-func _start_pulse_animation() -> void:
-	_pulse_tween = create_tween()
-	_pulse_tween.set_loops()
-	_pulse_tween.set_trans(Tween.TRANS_SINE)
-	_pulse_tween.set_ease(Tween.EASE_IN_OUT)
-	
-	_pulse_tween.tween_property(self, "scale", Vector2(1.15, 1.15), 0.5)
-	_pulse_tween.tween_property(self, "scale", Vector2(1.0, 1.0), 0.5)
-
-
 func _on_body_entered(body: Node2D) -> void:
+	if _is_collected:
+		return
+	
 	if body is Ball:
+		_is_collected = true
+		# Disable collision immediately to prevent double collection
+		set_deferred("monitoring", false)
+		
 		# Collect the star
 		GameManager.collect_star()
 		AudioManager.play_sfx(null)  # TODO: Add star collect sound
@@ -59,9 +59,6 @@ func _on_body_entered(body: Node2D) -> void:
 
 
 func _play_collect_animation() -> void:
-	if _pulse_tween:
-		_pulse_tween.kill()
-	
 	var tween = create_tween()
 	tween.set_trans(Tween.TRANS_BACK)
 	tween.set_ease(Tween.EASE_IN)
@@ -77,3 +74,4 @@ func _on_collect_finished() -> void:
 
 func set_speed(new_speed: float) -> void:
 	speed = new_speed
+
